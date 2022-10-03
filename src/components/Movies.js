95dfc2a65f45137f20c990e963b8d09125f009b9
Gmatieso <geoffrey.matieso@student.moringaschool.com>
2022-10-03 17:13:06 +0300

    :construction:  Using map to loop through set of data

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -1,8 +1,27 @@
 import React from "react";
 import { movies } from "../data";
 
+
 function Movies() {
-  return <div>{/*{code here}*/}</div>;
+// using map to loop through a set of data 
+  const myMovies = movies.map((movies) =>(
+    <div key={movies.index}>
+      <h2>{movies.title}</h2>
+    <p>Runtime: {movies.time} min.</p>
+
+    <ul>
+      {movies.genres.map((genre)=>
+      (<li key={genre.index}>{genre}</li>
+      ))}
+    </ul>
+    </div>
+  ))
+  return( 
+    <div>
+      <h1>Movies Page</h1>
+      {myMovies}
+    </div>
+  );
 }
 
 export default Movies;
