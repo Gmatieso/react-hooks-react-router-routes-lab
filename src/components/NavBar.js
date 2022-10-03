05ade6e83c1f49c2da94da4474090dace2f465c7
Gmatieso <geoffrey.matieso@student.moringaschool.com>
2022-10-03 15:56:25 +0300

    :construction:  Rendering NavLink Components

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,8 +1,35 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
 
+// Renders our four NavLinks  components i.e Home, Movies, Directors, and Actors     
 function NavBar() {
-  return <div>{/*{code here}*/}</div>;
+  return (
+  <div>
+    <NavLink
+    to ="/"
+    exact >
+      Home
+    </NavLink>
+
+    <NavLink
+    to ="/movies"
+    exact >
+      Movies
+    </NavLink>
+
+    <NavLink
+    to ="/directors"
+    exact >
+      Directors
+    </NavLink>
+
+    <NavLink
+    to ="/actors"
+    exact >
+      Actors
+    </NavLink>
+
+  </div>);
 }
 
 export default NavBar;
