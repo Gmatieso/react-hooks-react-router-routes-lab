a6c44b693b0844591925af8324a0f6a583ff454b
Gmatieso <geoffrey.matieso@student.moringaschool.com>
2022-10-03 16:01:18 +0300

    :construction: Rendering Text Home Page

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,11 @@
 import React from "react";
 
+//rendering the text home page 
+
 function Home() {
-  return <div>{/*{code here}*/}</div>;
+  return <div>
+    <h1>Home Page</h1>
+  </div>;
 }
 
 export default Home;
