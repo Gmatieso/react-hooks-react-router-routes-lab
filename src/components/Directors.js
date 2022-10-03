885b2dc181978c098983d19a47bf4d839b03db2d
Gmatieso <geoffrey.matieso@student.moringaschool.com>
2022-10-04 01:56:59 +0300

    :construction: mapping directors data

--- src/components/Directors.js
+++ src/components/Directors.js
@@ -2,7 +2,25 @@ import React from "react";
 import { directors } from "../data";
 
 function Directors() {
-  return <div>{/*{code here}*/}</div>;
+// making a new div for each director then mapping the data 
+const director = directors.map((directors)=>
+(<div key={directors.index}>
+  <h2>{directors.name}</h2>
+  <ul>
+    {directors.movies.map((movies)=>
+    <li key={movies.index}>
+      {movies}
+    </li>
+    )}
+  </ul>
+</div>
+
+))
+  return(
+  <div>
+    <h1>Directors  Page </h1> 
+    {director}
+  </div>);
 }
 
 export default Directors;
